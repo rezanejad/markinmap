53bde9f12917dfa2c3690fbfb6e70cd65745acf5
Asghar Rezanejad <a.rezanejad@gmail.com>
2023-06-04 10:34:34 +0330

    Version 1 - Final - Omit batch file

--- gitgit.bat
+++
@@ -1,5 +0,0 @@
-git config --global user.name "Asghar Rezanejad"
-git config --global user.email "a.rezanejad@gmail.com"
-git config --global push.default matching
-git config --global alias.co checkout
-git init
